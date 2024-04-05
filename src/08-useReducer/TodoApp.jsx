e4252fce53c725070bfa80d1b10446aac69128f9
Ignacio <jlopez@acl.cl>
2024-04-05 17:07:49 -0300

    [TODO] borrar

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,27 +1,52 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import { todoReducer } from "./TodoReducer";
 import { TodoList } from "./TodoList";
 import { TodoAdd } from "./TodoAdd";
 
 const initialState = [
-    {
-        id: new Date().getTime(),
-        description: 'Recolectar la piedra del alma',
-        done: false,
-    },
-    {
-        id: new Date().getTime() * 3,
-        description: 'Recolectar la piedra del tiempo',
-        done: false,
-    }
+    // {
+    //     id: new Date().getTime(),
+    //     description: 'Recolectar la piedra del alma',
+    //     done: false,
+    // }
 ]
+// esto dice que guardará en local storage la persistencia del objeto
+// al final de la linea || [] indica que si está vacio, devolverá vacio para 
+// evitar el null
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos')) || [];
+}
 
 export const TodoApp = () => {
 
-    const [ todos, dispatchTodo ] = useReducer( todoReducer, initialState );
+    const [ todos, dispatch ] = useReducer( todoReducer, initialState, init );
+    // guarda en el local storage
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify( todos ));
+    }, [todos])
+    
 
     const handleNewTodo = ( todo ) => {
-        console.log({ todo });
+        const action = {
+            type: '[TODO] Add todo',
+            payload: todo
+        }
+
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = ( id ) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
+    const handleToggleTodo = ( id ) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        });
     }
 
     return (
@@ -31,7 +56,11 @@ export const TodoApp = () => {
 
             <div className="row">
                 <div className="col-7">
-                    <TodoList todos={ todos }/>                    
+                    <TodoList 
+                        todos={ todos } 
+                        onDeleteTodo ={  handleDeleteTodo } 
+                        onToggleTodo={ handleToggleTodo }
+                    />                    
                 </div>
 
                 <div className="col-5">
--- src/08-useReducer/TodoReducer.js
+++ src/08-useReducer/TodoReducer.js
@@ -1,10 +1,28 @@
 
-export const todoReducer = ( initialState = [], action ) => {
+export const todoReducer = (initialState = [], action) => {
 
-    switch ( action.type ) {
-        case 'ABC':
-            throw new Error('Action.type = ABC no está implementada');
-    
+    switch (action.type) {
+        case '[TODO] Add todo':
+            return [...initialState, action.payload];
+
+        case '[TODO] Remove Todo':
+            //firter regresa un nuevo arreglo
+            // regresa todos los todos siempre y cuando el id sea diferente
+            return initialState.filter( todo => todo.id !== action.payload );
+
+        case '[TODO] Toggle Todo':
+            return initialState.map(  todo => {
+
+                if ( todo.id === action.payload ) {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+
+                return todo;
+            });
+
         default:
             return initialState;
     }
